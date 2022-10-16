64aa23e7b9f764977d5d7dcece7c36259ec0dc11
adityaarcot <arcot.aditya@utexas.edu>
2022-10-16 10:14:52 -0500

    code cleanup

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# api https://docs.streamlit.io/library/api-reference
+"""Provides optimal crop suggestions based on location using weather and soil data"""
 
-from dotenv import load_dotenv
 import os
 import time
-import numpy as np
 from datetime import date, timedelta
+import json
+
+from dotenv import load_dotenv
+import numpy as np
 import streamlit as st
 from streamlit_folium import st_folium
 import requests
-import json
 import folium
 from folium.plugins import MousePosition
 
@@ -24,30 +25,41 @@ ZOOM_LEVEL = 15
 
 @st.cache
 def read_data():
-    data = np.genfromtxt('Crop_recommendation.csv', delimiter=',', dtype=None, encoding=None)[1:, [3,4,5,7]]
+    """Read in reference data for optimal soil & weather conditions for crops"""
 
-    crops = data[:, -1]
-    data = data[:, :-1].astype('float32')
+    csv = np.genfromtxt('Crop_recommendation.csv', delimiter=',', \
+        dtype=None, encoding=None)[1:, [3,4,5,7]]
+
+    crops = csv[:, -1]
+    data = csv[:, :-1].astype('float32')
 
     return data, crops
 
-data, crops = read_data()
+data_table, crops_table = read_data()
 
 
 st.title("Crop Optimization By Location")
 st.write("Based on soil properties and weather conditions")
 
 
-def format_float(f):
-    return round(f, 2)
+def format_float(num):
+    """Round float to 2 decimals"""
+    return round(num, 2)
+
 
 def request(url, headers=None, params=None):
-    return requests.get(url, headers=headers, params=params)
+    """Request website data"""
+    return requests.get(url, headers=headers, params=params, timeout=15)
+
 
 def parse_response(resp):
+    """Convert website response to json"""
     return json.loads(resp.text)
 
+
 def valid_lat_lng(lat, lng):
+    """Check validity of lat & lng"""
+
     try:
         lat = float(lat)
         lng = float(lng)
@@ -64,6 +76,8 @@ def valid_lat_lng(lat, lng):
 
 
 def display_info(lat, lng):
+    """Show data and results after location data is obtained"""
+
     with st.spinner('Loading environment data...'):
         res = api_calls(lat, lng)
 
@@ -81,24 +95,26 @@ def display_info(lat, lng):
     with st.spinner('Computing suggested crops...'):
         time.sleep(1)
         vals = [temp, humidity, ph]
-        mse = (np.square(vals - data)).mean(axis=1)
+        mse = (np.square(vals - data_table)).mean(axis=1)
 
         suggestions = []
 
         idx = mse.argsort()
         i = 0
         while len(suggestions) < 3:
-            crop = crops[idx[i]]
+            crop = crops_table[idx[i]]
             if not crop in suggestions:
                 suggestions.append(crop)
             i += 1
 
         st.header('Suggested crops:')
-        for i in range(len(suggestions)):
-            st.write(i+1, ':', suggestions[i])
+        for ind, i in enumerate(suggestions):
+            st.write(ind+1, i)
 
 
 def api_calls(lat, lng):
+    """Make calls to get soil & weather data from APIs"""
+
     soil_headers = {'Content-type': "application/json"}
     soil_params = {
         "lat": lat,
@@ -111,12 +127,12 @@ def api_calls(lat, lng):
 
     try:
         ph = soil_parsed['properties']['layers'][0]['depths'][0]['values']['mean'] / 10
-    except:
+    except TypeError:
         return (False, "No pH data for this region!")
-        
 
     today = date.today()
     yesterday = today - timedelta(days = 1)
+
 
     url = f'https://api.ambeedata.com/weather/history/daily/by-lat-lng?lat={lat}&lng={lng}&' + \
         f'from={yesterday}%2000:00:00&to={today}%2000:00:00' + \
@@ -125,19 +141,21 @@ def api_calls(lat, lng):
 
     try:
         temp = ambee_parsed["data"]["history"][0]["temperature"]
-    except:
+    except TypeError:
         return (False, "No temperature data for this region!")
 
     try:
         humidity = ambee_parsed["data"]["history"][0]["humidity"] * 100
-    except:
+    except TypeError:
         return (False, "No humidity data for this region!")
 
     return (True, [ph, temp, humidity])
-        
+
 
 def handle_map():
-    m = folium.Map()
+    """Run when on map tab"""
+
+    map1 = folium.Map()
     formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
 
     MousePosition(position="topright",
@@ -145,26 +163,28 @@ def handle_map():
                     empty_string="NaN",
                     prefix="Coordinates:",
                     lat_formatter=formatter,
-                    lng_formatter=formatter).add_to(m)
+                    lng_formatter=formatter).add_to(map1)
 
-    m.add_child(folium.LatLngPopup())
-    map = st_folium(m, height=350, width=700)
+    map1.add_child(folium.LatLngPopup())
+    map_data = st_folium(map1, height=350, width=700)
 
-    if map['last_clicked']:
-        lat, lng = map['last_clicked']['lat'], map['last_clicked']['lng']
+    if map_data['last_clicked']:
+        lat, lng = map_data['last_clicked']['lat'], map_data['last_clicked']['lng']
         display_info(lat, lng)
 
 
 def handle_address():
+    """Run when on address tab"""
+
     address = st.text_input('Enter address')
 
-    # TODO: keep getting this error 'NoneType' object has no attribute 'split'
     if address:
-        suggestions_url = f"https://maps.googleapis.com/maps/api/place/autocomplete/" + \
+        suggestions_url = "https://maps.googleapis.com/maps/api/place/autocomplete/" + \
             f"json?input={'%20'.join(address.split())}" + \
             f"&key={GOOGLE_MAPS_API_KEY}"
 
-        options = [i['description'] for i in parse_response(request(suggestions_url))['predictions']]
+        options = [i['description'] for i in \
+            parse_response(request(suggestions_url))['predictions']]
 
         if len(options) > 0:
             address = st.selectbox('suggestions', options)
@@ -180,16 +200,18 @@ def handle_address():
                 st.write('Latitude & longitude could not be found!')
                 return
 
-            m2 = folium.Map([lat, lng], zoom_start=ZOOM_LEVEL)
-            folium.Marker([lat, lng]).add_to(m2)
-            map2 = st_folium(m2, height=350, width=700)
+            map2 = folium.Map([lat, lng], zoom_start=ZOOM_LEVEL)
+            folium.Marker([lat, lng]).add_to(map2)
+            st_folium(map2, height=350, width=700)
             display_info(lat, lng)
-        
+
         else:
             st.write('Invalid address, please try again!')
 
 
 def handle_latlng():
+    """Run when on lat/lng tab"""
+
     col1, col2 = st.columns(2)
     with col1:
         lat = st.text_input('Enter latitude')
@@ -200,10 +222,10 @@ def handle_latlng():
         if not valid_lat_lng(lat, lng):
             st.write('invalid values - try again')
             return
-            
-        m3 = folium.Map([lat, lng], zoom_start=ZOOM_LEVEL)
-        folium.Marker([lat, lng]).add_to(m3)
-        map3 = st_folium(m3, height=350, width=700)
+
+        map3 = folium.Map([lat, lng], zoom_start=ZOOM_LEVEL)
+        folium.Marker([lat, lng]).add_to(map3)
+        st_folium(map3, height=350, width=700)
         display_info(lat, lng)
 
 
