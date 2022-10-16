c0c325ce599a550e939f53c2737ab10bf67163c1
adityaarcot <arcot.aditya@utexas.edu>
2022-10-16 03:40:57 -0500

    Abstracted repetitive code, added loading for API calls

--- main.py
+++ main.py
@@ -23,68 +23,96 @@ AMBEE_API_KEY = os.getenv('AMBEE_API_KEY')
 
 AMBEE_URL = "https://api.ambeedata.com/weather/latest/by-lat-lng"
 SOIL_URL = "https://rest.isric.org/soilgrids/v2.0/properties/query"
+ZOOM_LEVEL = 15
 
 
-st.title("Web app!")
+st.title("Title placeholder")
+
+
+
+def format_float(f):
+    return round(f, 2)
+
+def request(url, headers=None, params=None):
+    return requests.get(url, headers=headers, params=params)
+
+def parse_response(resp):
+    return json.loads(resp.text)
+
+def valid_lat_lng(lat, lng):
+    try:
+        lat = float(lat)
+        lng = float(lng)
+    except ValueError:
+        return False
+
+    if lat < -90 or lat > 90:
+        return False
+
+    if lng < -180 or lng > 180:
+        return False
+
+    return True
 
 
 def display_info(lat, lng):
+    with st.spinner('Loading data...'):
+        res = api_calls(lat, lng)
 
-    ph, temperature, humidity = api_calls(lat, lng)
+    if res[0]: #success
+        ph, temp, humidity = res[1]
+        st.write('pH:', format_float(ph))
+        st.write('Temperature:', format_float(temp))
+        st.write('Humidity:', format_float(humidity))
+    else:
+        st.write(res[1])
+        return
 
-    try:
+    '''try:
         crop_recommendations = crop_recommender_model(ph, temperature, humidity)
         st.write(crop_recommendations)
     except Exception as e:
         print(e)
         error = "Encountered error when training model or creating recommendation"
-        st.write(error)
+        st.write(error)'''
+
+
 
 
 def api_calls(lat, lng):
-    ambee_querystring = {"lat": lat, "lng": lng}
-    soil_querystring = {
+    soil_headers = {'Content-type': "application/json"}
+    soil_params = {
         "lat": lat,
         "lon": lng,
         'property': ['phh2o'],
         'depth': ['0-5cm'],
         'value': 'mean'
     }
+    soil_parsed = parse_response(request(SOIL_URL, soil_headers, soil_params))
 
     try:
-        response = requests.get(SOIL_URL,
-                                headers={'Content-type': "application/json"},
-                                params=soil_querystring)
-        soil_parsed = json.loads(response.text)
         ph = soil_parsed['properties']['layers'][0]['depths'][0]['values']['mean'] / 10
-        st.write('PH:', ph)
-
-    except Exception:
-        soil_error = "No ph data for this region"
-        st.write('Error:', soil_error)
-      
+    except:
+        return (False, "No pH data for this region!")
+        
     ambee_headers = {
         'x-api-key': AMBEE_API_KEY,
         'Content-type': "application/json"
     }
+    ambee_params = {"lat": lat, "lng": lng}
+    ambee_parsed = parse_response(request(AMBEE_URL, ambee_headers, ambee_params))
 
     try:
-        response = requests.request("GET",
-                                    AMBEE_URL,
-                                    headers=ambee_headers,
-                                    params=ambee_querystring)
-        ambee_parsed = json.loads(response.text)
+        temp = (ambee_parsed['data']['temperature'] - 32) * 5 / 9
+    except:
+        return (False, "No temperature data for this region!")
 
-        temperature = (ambee_parsed['data']['temperature'] - 32) * 5 / 9
-        humidity = ambee_parsed['data']['humidity']
-        st.write('Temperature:', temperature)
-        st.write('Humidity:', humidity)
+    try:
+        humidity = ambee_parsed['data']['humidity'] * 100
+    except:
+        return (False, "No humidity data for this region!")
 
-    except Exception:
-        ambee_error = "No temperature and/or humidity data for this region"
-        st.write('Error', ambee_error)
-
-    return ph, temperature, humidity
+    return (True, [ph, temp, humidity])
         
 
 def crop_recommender_model(ph, temp, humidity):
@@ -105,10 +133,8 @@ def matricies(df, args: list):
     return X, y
 
 
-map_option, address_option, latlng_option = st.tabs(["Map", "Address", "Lat / Long"])
-
-with map_option:
-    m = folium.Map(zoom_start=20)
+def handle_map():
+    m = folium.Map()
     formatter = "function(num) {return L.Util.formatNum(num, 3) + ' º ';};"
 
     MousePosition(position="topright",
@@ -125,70 +151,57 @@ with map_option:
         lat, lng = map['last_clicked']['lat'], map['last_clicked']['lng']
         display_info(lat, lng)
 
-with address_option:
-    address = st.text_input('input address')
+
+def handle_address():
+    address = st.text_input('Enter address')
 
     if address:
-        url = f"https://maps.googleapis.com/maps/api/place/autocomplete/" + \
+        suggestions_url = f"https://maps.googleapis.com/maps/api/place/autocomplete/" + \
             f"json?input={'%20'.join(address.split())}" + \
             f"&key={GOOGLE_MAPS_API_KEY}"
 
-        resp = requests.request("GET", url)
-        text = json.loads(resp.text)
-
-        options = [i['description'] for i in text['predictions']]
-
+        options = [i['description'] for i in parse_response(request(suggestions_url))['predictions']]
         address = st.selectbox('suggestions', options)
 
-        url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + \
+        address_url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + \
             '%20'.join(address.split()) + \
             f"&key={GOOGLE_MAPS_API_KEY}"
 
-        resp = requests.request("GET", url)
-        text = json.loads(resp.text)
-
+        latlng = parse_response(request(address_url))['results'][0]['geometry']['location']
         try:
-            latlng = text['results'][0]['geometry']['location']
             lat, lng = latlng['lat'], latlng['lng']
-
-            m2 = folium.Map([lat, lng], zoom_start=15)
-            folium.Marker([lat, lng]).add_to(m2)
-            map2 = st_folium(m2, height=350, width=700)
-
-            display_info(lat, lng)
-
         except IndexError:
-            print('lat lng not found')
+            st.write('Latitude & longitude could not be found!')
+            return
 
-def check_lat_lng(lat, lng):
-    try:
-        lat = float(lat)
-        lng = float(lng)
-    except ValueError:
-        return False
-
-    if lat < -90 or lat > 90:
-        return False
-
-    if lng < -180 or lng > -180:
-        return False
-
-    return True
+        m2 = folium.Map([lat, lng], zoom_start=ZOOM_LEVEL)
+        folium.Marker([lat, lng]).add_to(m2)
+        map2 = st_folium(m2, height=350, width=700)
+        display_info(lat, lng)
 
 
-with latlng_option:
+def handle_latlng():
     col1, col2 = st.columns(2)
     with col1:
-        lat = st.text_input('input lat')
+        lat = st.text_input('Enter latitude')
     with col2:
-        lng = st.text_input('input lng')
+        lng = st.text_input('Enter longitude')
 
     if lat and lng:
-        if not check_lat_lng(lat, lng):
+        if not valid_lat_lng(lat, lng):
             st.write('invalid values - try again')
-        else:
-            m3 = folium.Map([lat, lng], zoom_start=15)
-            folium.Marker([lat, lng]).add_to(m3)
-            map3 = st_folium(m3, height=350, width=700)
+            return
+            
+        m3 = folium.Map([lat, lng], zoom_start=ZOOM_LEVEL)
+        folium.Marker([lat, lng]).add_to(m3)
+        map3 = st_folium(m3, height=350, width=700)
+        display_info(lat, lng)
 
-            display_info(lat, lng)
+
+map_option, address_option, latlng_option = st.tabs(["Map", "Address", "Lat / Long"])
+with map_option:
+    handle_map()
+with address_option:
+    handle_address()
+with latlng_option:
+    handle_latlng()
